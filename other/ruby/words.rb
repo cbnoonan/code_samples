af40b044e7ad413861df16846b3dc1c3c5883269
Colleen Noonan <cbnoonan@gmail.com>
2011-02-24 19:05:53 -0800

    got it working yay. now it is slow

--- other/ruby/words.rb
+++ other/ruby/words.rb
@@ -1,6 +1,7 @@
 
-
+$tryWordList ={} 
 class MyClass
+
   def readFileAndSort() 
      f = File.open("words.txt") or die "Unable to open file..."
 
@@ -11,8 +12,6 @@ class MyClass
 
      sortedList =  contentsArray.sort_by{|x| x.length}.reverse
 
-#     puts sortedList.inspect
-
      puts "Calculating..."
 
      getLongestWord(sortedList)
@@ -21,48 +20,70 @@ class MyClass
   # find longest word and return it
   def getLongestWord(words)
     puts "size:  #{words.length}"
-    word = ''
-    testWord = ''
-    validWord = ''
-    candidates = []
-    permutation = ''
+    longestSoFar = ''
 
+    for i in 0..(words.length-1)
+        tryCount = 0
+        longestSoFar = words[i].chomp
 
-    catch (:done) do
-        for i in 0..words.length
-            word = words[i].chomp!
-            testWord = word
-#            puts "1: testword:  #{testWord}"
-
-            for j in i+1..words.length
+        for j in i+1..(words.length-1)
             # iterate through every word in the list, setting original value to testWord
             # then find if other strings (words in the file) occur
-                puts "1. testword #{testWord} value j: #{words[j]}"
-                testWord = testWord.gsub(words[j].chomp!,'') 
-                puts "2: testword:  #{testWord}"
+#            puts "i: #{i} j: #{j} testword #{words[i].chomp} value j: #{words[j].chomp!} longestSoFar: #{longestSoFar}"
 
-                if testWord.length == 1
-                    #  too short, replace original word and carry on
-                    testWord = word
-                end
-                if testWord.length == 0
-                   validWord = word
-#                   isValidWord dd= True
-                    throw :done  
-#                   break
-                 end
+            if longestSoFar.include? words[j]
+#               puts "AWESOME. words[j] is in longestSoFar"
+               $tryWordList[tryCount+=1] = words[j]
             end
-        end
+         end
+#         puts "trywordlist " 
+#         p $tryWordList
+         longestSoFar = remove_leader(longestSoFar, $tryWordList, tryCount)
+         if longestSoFar == nil
+            puts "winner ${words[i]}"
+            stop = Time.new
+            totalTime = stop.to_f - start.to_f 
+            puts "total time : #{totalTime}"
+            return EXIT_SUCCESS
+         end
     end
-    puts "valid word: #{validWord}"
-#   MethodFactory methodFactory = new MethodFactory(intMethod);
-#        String strValidWord = methodFactory.objMethod.getLongestWord(lstWords);
- 
-#        return strValidWord;
+    return EXIT_FAILURE
   end
+
+  def remove_leader(longestSoFar, candidates, tryCount)
+    puts "longest so far: #{longestSoFar}"
+    puts "try count : #{tryCount}"
+    puts "candidates : "
+    p candidates
+#    puts "tryWordList kys: #{$tryWordList.keys}"
+#    puts "tryWordList: #{$tryWordList}"
+    longest = longestSoFar
+    if longestSoFar.length == 0
+        return nil
+    end 
+#    for i in 0..(tryCount-1) 
+    for i in 1..tryCount 
+#       puts "length of i word #{$tryWordList[i].length}"
+       len = $tryWordList[i].length 
+       puts "longesSoFar length #{longestSoFar[0..len]}"
+       puts "tryword list #{$tryWordList[i][0..len]}"
+       if (longestSoFar[0..len] <=> $tryWordList[i][0..len]) == 0  
+          puts "lengths are equal"
+          longest = remove_leader(longestSoFar + len, candidates, tryCount)
+          if longest == nil
+             return longest
+          end
+       else
+          puts "lengths are not equal"
+       end
+    end
+    return longest
+  end
+  
 
 end
 
+start = Time.new
 
 c = MyClass.new()
 c.readFileAndSort
